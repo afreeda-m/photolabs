72d4dbb39a0da14e4dbd43d1f478de9421eda0fa
Afreeda <64160666+afreeda-m@users.noreply.github.com>
2023-09-15 10:09:16 -0400

    move child components into parent Top Navigation bar component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 import PhotoList from 'components/PhotoList';
-import TopicList from 'components/TopicList';
 import './App.scss';
+import TopNavigation from 'components/TopNavigationBar';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      <TopicList/>
+      <TopNavigation/>
       <PhotoList/>
     </div>
   );
