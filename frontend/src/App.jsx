8448d67479f209d7c4947dea5a278842f81be639
Afreeda <64160666+afreeda-m@users.noreply.github.com>
2023-09-19 20:04:37 -0400

    add functionality to close modal and center modal into the view

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
   return (
     <div className="App">
       <HomeRoute setIsModalOpen={setIsModalOpen}/>
-      {isModalOpen && <PhotoDetailsModal />}
+      {isModalOpen && <PhotoDetailsModal setIsModalOpen={setIsModalOpen}/>}
     </div>
   );
 };
